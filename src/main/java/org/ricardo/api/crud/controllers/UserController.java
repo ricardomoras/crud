49c0cf3ebdd171c93c013f4da35a3375d67c27b2
ricardomoras <ricardo.mora.sanchez@gmail.com>
2024-09-10 17:22:27 -0600

    before test

--- src/main/java/org/ricardo/api/crud/controllers/UserController.java
+++ src/main/java/org/ricardo/api/crud/controllers/UserController.java
@@ -1,11 +1,17 @@
 package org.ricardo.api.crud.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.ricardo.api.crud.models.UserModel;
 import org.ricardo.api.crud.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +27,31 @@ public class UserController {
 		return this.userService.getUsers();
 	}
 	
+	@PostMapping
+	public UserModel saveUser(@RequestBody UserModel user) {
+		return this.userService.saveUser(user);
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<UserModel> getUserById(@PathVariable Long id){
+		return this.userService.getById(id);
+	}
+	
+	@PutMapping
+	public UserModel updateUserById(@RequestBody UserModel request,@PathVariable Long id) {
+		return userService.updateById(request, id);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public String deleteById(@PathVariable Long id) {
+		
+		boolean ok = userService.deleteUser(id);
+		if(ok) {
+			return "User with id " + id + " deleted!";
+		} else {
+			return "Error, we have a problem and can't delete user";
+		}
+	}
 	
 
 }
--- src/main/java/org/ricardo/api/crud/services/UserService.java
+++ src/main/java/org/ricardo/api/crud/services/UserService.java
@@ -1,6 +1,7 @@
 package org.ricardo.api.crud.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.ricardo.api.crud.models.UserModel;
 import org.ricardo.api.crud.repositories.IUserRepository;
@@ -15,5 +16,33 @@ public class UserService {
 	
 	public List<UserModel> getUsers(){
 		return (List<UserModel>) userRepository.findAll();
+	}
+	
+	public UserModel saveUser(UserModel user) {
+		return userRepository.save(user);
+	}
+	
+	public Optional<UserModel> getById(Long id){
+		return userRepository.findById(id);
+	}
+	
+	public UserModel updateById(UserModel request,Long id){
+		UserModel user = userRepository.findById(id).get();
+		
+		user.setFirtName(request.getFirtName());
+		user.setLastName(request.getLastName());
+		user.setEmail(request.getEmail());
+		
+		return user;
+	}
+	
+	public Boolean deleteUser(Long id) {
+		try {
+			userRepository.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+		
 	}
 }
